c2f115453966e79ecfa1145e4ab0ac5d6a6097b1
WDControl <jerome.celebi@ynov.com>
2020-11-15 14:58:07 +0100

    Updating Scorer

--- src/components/Scorer.vue
+++ src/components/Scorer.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="scorer">{{ vote_average }}</div>
+  <div :class="`scorer ${color}`">{{ vote_average }}</div>
 </template>
 
 <script>
 export default {
   props: {
     vote_average: Number,
+  },
+  computed: {
+    color() {
+      return this.vote_average >= 7 ? "green" : "yellow";
+    },
   },
 };
 </script>
@@ -15,12 +20,20 @@ export default {
   color: #fff;
   position: absolute;
   right: 30px;
-  background-color: green;
   border-radius: 50%;
   width: 30px;
   height: 30px;
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.green {
+  background-color: green;
+}
+
+.yellow {
+  background-color: yellow;
+  color: #000;
 }
 </style>
